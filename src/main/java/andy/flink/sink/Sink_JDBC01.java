abb1a258f4d23482c1aeb969a67e3d2eb616110b
lichao <lichao@rgyun.com>
2021-05-23 16:32:37 +0800

    flink_java

--- src/main/java/andy/flink/sink/Sink_JDBC01.java
+++ src/main/java/andy/flink/sink/Sink_JDBC01.java
@@ -15,7 +15,7 @@ import java.sql.PreparedStatement;
 
 public class Sink_JDBC01 {
     public static void main(String[] args) throws Exception {
-        //TODO 1.创建环境
+        //TODO  1.创建环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
